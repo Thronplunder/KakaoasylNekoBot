e0faeb85868f81ab2af081c5ad0b3efeebd1f524
Arvedui <arvedui@posteo.de>
2018-11-08 09:34:36 +0100

    add 8ball command

--- bot.py
+++ bot.py
@@ -32,6 +32,29 @@ BOOB_NIPPLES = (
         "o", ".", "O", "0", "。", "+", "p", "-", "*", "•", "^", "°", "○",
         )
 
+MAGIC_8_BALL_ANSWERS = (
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful.",
+            )
+
 
 # flask app
 app = Flask(__name__)
@@ -117,6 +140,9 @@ def getButt():
         return (True, absolute_preview_url)
     else:
         return (False, "Butts are striking :(")
+
+def get8Ball():
+    return random.choice(MAGIC_8_BALL_ANSWERS)
 
 
 # post help
@@ -209,4 +235,7 @@ def handle():
                         sendImage(chatID, content)
                     else:
                         sendMessage(chatID, content)
+
+                if '/8ball' in message:
+                    sendMessage(chatID, get8Ball())
     return "ok"
